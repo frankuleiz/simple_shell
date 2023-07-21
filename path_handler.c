fdee57ebad30234b172f702d324cdc6d41645310
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-07-21 21:28:58 +0300

    path finder

---
+++ path_handler.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+/**
+ *
+ *
+ *
+ */
+char *find_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *dir, *full_path;
+	struct stat st;
+
+	if (path == NULL)
+	{
+		return (NULL);
+	}
+	dir = strtok(path, ":");
+
+	while (dir != NULL)
+	{
+		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path,command);
+
+		if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode) && access(full_path, X_OK) == 0)
+		{
+			return (full_path);
+		}
+
+		free (full_path);
+		dir = strtok(NULL, ":");
+	}
+	return (NULL);
+}
--- shell.h
+++ shell.h
@@ -7,8 +7,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #define BUFFER_SIZE 1024
 void execute_command(char *command);
 char **parse_args(char *command);
+char *find_path(char *command);
 
 #endif
