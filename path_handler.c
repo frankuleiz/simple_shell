e3e0779b2c177436fcc40f5ddba9153ccb221e82
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-07-24 22:56:17 +0300

    path_handler

--- path_handler.c
+++ path_handler.c
@@ -8,35 +8,42 @@
 char *find_path(char *command)
 {
 	char *path = getenv("PATH");
-	char *dir, *full_path;
+	char *dir_start, *dir_end, *full_path;
 	struct stat st;
+	size_t dir_len;
 
 	if (path == NULL)
 	{
 		return (NULL);
 	}
-	dir = strtok(path, ":");
-
-	while (dir != NULL)
+	dir_start = path;
+	while (*dir_start)
 	{
-		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		dir_end = strchr(dir_start, ':');
+		if (dir_end == NULL)
+		{
+			dir_end = dir_start + strlen(dir_start);
+		}
+		dir_len = dir_end - dir_start;
+		full_path = malloc(dir_len + 1 + strlen(command) + 1);
 		if (full_path == NULL)
 		{
 			perror("malloc");
 			exit(EXIT_FAILURE);
 		}
-
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-
+		strncpy(full_path, dir_start, dir_len);
+		full_path[dir_len] = '/';
+		strncpy(full_path + dir_len + 1, command, strlen(command) + 1);
 		if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode) && access(full_path, X_OK) == 0)
 		{
 			return (full_path);
 		}
-
 		free(full_path);
-		dir = strtok(NULL, ":");
+		dir_start = dir_end;
+		if (*dir_start == ':')
+		{
+			dir_start++;
+		}
 	}
 	return (NULL);
 }
