49b73ec58f00099b075a77d76cadacad035f2278
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-07-27 19:15:17 +0300

    correction

--- exec.c
+++ exec.c
@@ -1,58 +1,50 @@
 #include "shell.h"
 /**
- * execute_command - execute a given command
- * @command: the command to be executed
+ *execute_command - execute a given command
+ *@command: the command to be executed
  */
 void execute_command(char *command)
 {
-	int status;
-	char **args, *full_path;
-	pid_t pid;
+        int status;
+        char **args;
+        pid_t pid;
 
-	if (strcmp(command, "exit") == 0)
-	{
-		char *arg = strchr(command, ' ');
+        if (strcmp(command, "exit") == 0)
+           {
+              char *arg = strchr(command, ' ');
 
-		_exit((arg != NULL) ? atoi(arg + 1) : 0);
-	}
-	if (strcmp(command, "env") == 0)
-	{
-		execute_env();
-		return;
-	}
+              if (arg != NULL)
+                {
+                   status = atoi(arg + 1);
+                } else {
+                   status = 0;
+                }
+              _exit(status);
+    }
 
-	full_path = search_path(command);
-
-	if (full_path == NULL)
-	{
-		char error_msg[] = "command not found: ";
-
-		write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-		write(STDERR_FILENO, command, strlen(command));
-		write(STDERR_FILENO, "\n", 1);
-		return;
-	}
-
-	args = parse_args(command);
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(args);
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-	{
-		execve(full_path, args, NULL);
-		perror(full_path);
-		free(args);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(&status);
-		free(args);
-	}
-	free(full_path);
+        if (strcmp(command, "env") == 0)
+        {
+                execute_env();
+                return;
+        }
+        args = parse_args(command);
+        pid = fork();
+        if (pid == -1)
+        {
+                perror("fork");
+                free(args);
+                exit(EXIT_FAILURE);
+        }
+        if (pid == 0)
+        {
+                execve(command, args, NULL);
+                perror(command);
+                free(args);
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                wait(&status);
+                free(args);
+        }
 }
-
